bf8c9c2233144c18607a5d7e0ea769be8210b9ab
Belenn <belennn25026@gmail.com>
2024-05-15 10:35:17 -0300

    Arregle ls conexion a la base de datos

--- peliculas/__init__.py
+++ peliculas/__init__.py
@@ -1,12 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+with app.app_context():
+    from . import db
+    db.init_app(app)
 
 @app.route('/')
 def hello():
     return 'Hello, World!'
 
-@app.route('quien')
-def quien():
-    return 'YO'
+@app.route('/inventory2')
+def inventory_id():
+    consulta = """SELECT name FROM genres ORDER BY name;
+    """
+    con = db.get_db()
+    res = con.execute(consulta)
+    lista_lenguajes = res.fetchall()
+    pagina = render_template('inventory.html', lenguajes=lista_lenguajes)
+    return pagina
+
+
+@app.route('/inventory')
+def inventory():
+    consulta = """SELECT last_update FROM inventory ORDER BY name;
+    """
+    con = db.get_db()
+    res = con.execute(consulta)
+    lista_inventory = res.fetchall()
+    pagina = render_template('inventory.html')
+    return pagina
+
+
+
+     
+     
--- peliculas/db.py
+++ peliculas/db.py
@@ -16,7 +16,7 @@ def dict_factory(cursor, row):
 def get_db():
     if 'db' not in g:
         g.db = sqlite3.connect(
-            current_app.config['DATABASE'],
+            db_file,
             detect_types=sqlite3.PARSE_DECLTYPES
         )
         g.db.row_factory = dict_factory
@@ -31,9 +31,14 @@ def close_db(e=None):
         db.close()
 
 def init_db():
+    try:
+        os.makedirs(db_folder)
+    except OSError:
+        pass
+
     db = get_db()
 
-    with current_app.open_resource('schema.sql') as f:
+    with current_app.open_resource(db_sql_file) as f:
         db.executescript(f.read().decode('utf8'))
 
 
