dd5abc63554764477c341d183ce8aebf7b43141c
Belenn <belennn25026@gmail.com>
2024-05-15 11:19:03 -0300

    Puse los diferentes idiomas en la pagina

--- peliculas/__init__.py
+++ peliculas/__init__.py
@@ -10,26 +10,16 @@ with app.app_context():
 def hello():
     return 'Hello, World!'
 
-@app.route('/inventory2')
-def inventory_id():
-    consulta = """SELECT name FROM genres ORDER BY name;
+@app.route('/lenguaje')
+def lenguaje():
+    consulta = """SELECT name FROM language ORDER BY name;
     """
     con = db.get_db()
     res = con.execute(consulta)
     lista_lenguajes = res.fetchall()
-    pagina = render_template('inventory.html', lenguajes=lista_lenguajes)
+    pagina = render_template('lenguaje.html', lenguajes=lista_lenguajes)
     return pagina
 
-
-@app.route('/inventory')
-def inventory():
-    consulta = """SELECT last_update FROM inventory ORDER BY name;
-    """
-    con = db.get_db()
-    res = con.execute(consulta)
-    lista_inventory = res.fetchall()
-    pagina = render_template('inventory.html')
-    return pagina
 
 
 
